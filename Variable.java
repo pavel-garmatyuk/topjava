d3d77e2bbe046c246861a145178087bb87aa746b
Pavel Garmatyuk <m-krum@yandex.ru>
2021-08-30 15:02:49 +0300

    first commit

---
+++ Variable.java
@@ -0,0 +1,22 @@
+public class Variable {   
+    public static void main(String[] args) {
+        String cpuName = "Intel";
+        byte core = 4;
+        short cpuGen = 4;
+        String cpuArhitecture = "Haswell";
+        int cpuModel = 5;
+        float cpuHz = 2.51F;
+        double cpuMaxHz = 3.4D;
+        boolean grathicCore = true;
+        char coreName = 'i';
+    System.out.println(cpuName);
+    System.out.println(core);
+    System.out.println(cpuGen);
+    System.out.println(cpuArhitecture);
+    System.out.println(cpuModel);
+    System.out.println(cpuHz);
+    System.out.println(cpuMaxHz);
+    System.out.println(grathicCore);
+    System.out.println(coreName);
+    }
+}
