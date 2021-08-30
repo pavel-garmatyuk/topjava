6d77d5af5bbfec48f35f30649373f96fa21989fb
Pavel Garmatyuk <m-krum@yandex.ru>
2021-08-30 20:22:48 +0300

    Fixed

---
+++ ConditionalStatement.java
@@ -0,0 +1,33 @@
+public class ConditionalStatement {   
+    public static void main(String[] args) {
+        int age = 21;
+        boolean manSex = true;
+        double height = 1.79;
+        char firstCharSet = 'M';
+        if (age > 20) {
+        System.out.println("You age > 20");
+}
+
+if (manSex) {
+    System.out.println("You man");
+}
+
+if(!manSex) {
+    System.out.println("You wooman");
+}
+
+if(height < 1.80) {
+    System.out.println("You large");
+} else {
+    System.out.println("You short");
+}
+
+if(firstCharSet == 'M') {
+    System.out.println("M");
+} if else (firstCharSet == 'I') {
+    System.out.println("I");
+} else {
+    System.out.println("Other");
+} 
+    }
+}
