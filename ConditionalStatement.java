f746ce3f884d4b4030403482cadb3548485dd601
Pavel Garmatyuk <m-krum@yandex.ru>
2021-09-02 09:26:28 +0300

    Update ConditionalStatement.java

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -8,26 +8,26 @@ public class ConditionalStatement {
         System.out.println("You age > 20");
 }
 
-if (manSex) {
-    System.out.println("You man");
+        if (manSex) {
+        System.out.println("You man");
 }
 
-if(!manSex) {
-    System.out.println("You wooman");
+        if(!manSex) {
+        System.out.println("You wooman");
 }
 
-if(height < 1.80) {
-    System.out.println("You large");
-} else {
-    System.out.println("You short");
+        if(height < 1.80) {
+        System.out.println("You large");
+        } else {
+        System.out.println("You short");
 }
 
-if(firstCharSet == 'M') {
-    System.out.println("M");
-} if else (firstCharSet == 'I') {
-    System.out.println("I");
-} else {
-    System.out.println("Other");
+        if(firstCharSet == 'M') {
+        System.out.println("M");
+        } if else (firstCharSet == 'I') {
+        System.out.println("I");
+        } else {
+        System.out.println("Other");
 }
 }
 }
