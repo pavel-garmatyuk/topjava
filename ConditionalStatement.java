482b44a07e7f901a9cb3f5b6ae7669f0978b8b36
Pavel Garmatyuk <m-krum@yandex.ru>
2021-09-07 10:49:41 +0300

    edited curly braces

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -2,28 +2,28 @@ public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 21;
         if (age > 20) {
-                System.out.println("You age > 20");
-}
+            System.out.println("You age > 20");
+        }
         boolean male = true;
         if (male) {
-                System.out.println("You man");
-}
+            System.out.println("You man");
+        }
         if(!male) {
-                System.out.println("You wooman");
-}
+            System.out.println("You wooman");
+        }
         double height = 1.79;
         if(height < 1.80) {
-                System.out.println("You large");
-                } else {
-                System.out.println("You short");
-}
+            System.out.println("You large");
+        } else {
+            System.out.println("You short");
+        }
         char firstLetterName = 'M';
         if(firstLetterName == 'M') {
-                System.out.println("M");
-                } if else (firstLetterName == 'I') {
-                System.out.println("I");
-                } else {
-                System.out.println("Other");
+            System.out.println("M");
+        } if else (firstLetterName == 'I') {
+            System.out.println("I");
+        } else {
+            System.out.println("Other");
         }
     }
 }
