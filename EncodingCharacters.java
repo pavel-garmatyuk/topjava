843bffd101fad5c716c1d35634a0ffc6ad25ebf3
Pavel Garmatyuk <m-krum@yandex.ru>
2021-09-07 12:57:44 +0300

    EncodingCharacters fixed

--- EncodingCharacters.java
+++ EncodingCharacters.java
@@ -1,10 +1,9 @@
 public class EncodingCharacters {
     public static void main(String[] args) {
-        char rangeStart = 9398;
-        char rangeEnd = 10178;
-        for (int i = rangeStart; i < rangeEnd; i++) {
-            char result = (char) i;
-            System.out.println(result);
+        char startRange = 9398;
+        char endRange = 10178;
+        for (int i = startRange; i < endRange; i++) {
+            System.out.println((char) i);
         }
     }
 }
