c5282732afcbf7eb467493d5c74c345bbb2d3ab0
Pavel Garmatyuk <m-krum@yandex.ru>
2021-09-07 17:46:11 +0300

    ConditionalStatement fixed spaces

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -4,19 +4,23 @@ public class ConditionalStatement {
         if (age > 20) {
             System.out.println("You age > 20");
         }
+
         boolean male = true;
         if (male) {
             System.out.println("You man");
         }
+
         if(!male) {
             System.out.println("You wooman");
         }
+
         double height = 1.79;
         if(height < 1.80) {
             System.out.println("You large");
         } else {
             System.out.println("You short");
         }
+
         char firstLetterName = 'M';
         if(firstLetterName == 'M') {
             System.out.println("M");
