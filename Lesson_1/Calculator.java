767f79ec24e503e363c43878570eef6de77dee24
Pavel Garmatyuk <m-krum@yandex.ru>
2021-09-07 19:21:23 +0300

    mv to new folder

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,29 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Введите первый операнад, затем знак арифметической операции, и второй операнд.\n"
+                + "Разрешенные знаки арифметической операции: \nСложение + \nВычитание - \nУмножение * \nДеление / \nВозвести в степень ^ \nПолучить остаток от деления %");
+        Scanner console = new Scanner(System.in);
+        int operandOne = console.nextInt();
+        String symbol = console.nextLine();
+        int operandTwo = console.nextInt();
+        int result = 0;
+        if (symbol.equals("+")) {
+            result = operandOne + operandTwo;
+        } else if (symbol.equals("-")) {
+            result = operandOne - operandTwo;
+        } else if (symbol.equals("*")) {
+            result = operandOne * operandTwo;
+        } else if (symbol.equals("/")) {
+            result = operandOne / operandTwo;
+        } else if (symbol.equals("%")) {
+            result = operandOne % operandTwo;
+        } else if (symbol.equals("^")) {
+            result = 1;
+            for (int i = 1; i <= operandTwo; i++) {
+                result *= operandOne;
+            }
+        }
+        System.out.println(result);
+    }
+}
---
+++ Lesson_1/Variable.java
@@ -0,0 +1,19 @@
+public class Variable {   
+    public static void main(String[] args) {
+        byte core = 4;
+        short cpuGen = 4;
+        int cpuModel = 5;
+        float cpuHz = 2.51F;
+        double cpuMaxHz = 3.4D;
+        boolean grathicCore = true;
+        char coreName = 'i';
+
+        System.out.println(core);
+        System.out.println(cpuGen);
+        System.out.println(cpuModel);
+        System.out.println(cpuHz);
+        System.out.println(cpuMaxHz);
+        System.out.println(grathicCore);
+        System.out.println(coreName);
+    }
+}
