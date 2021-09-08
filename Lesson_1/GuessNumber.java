ee7acacf40424fecde348f3155d0a581560b6871
Pavel Garmatyuk <m-krum@yandex.ru>
2021-09-08 13:30:22 +0300

    fixed else if, fixed while

--- Lesson_1/GuessNumber.java
+++ Lesson_1/GuessNumber.java
@@ -2,20 +2,18 @@ import java.util.Scanner;
 
 public class GuessNumber {
     public static void main(String[] args) {
-        int secretNumber = (int) Math.round(Math.random() * 100);
+        int secretNumber = (int) ((Math.random() * 100) + 1);
         System.out.println("Угадай число от 0 до 100.");
-        int inputNumber;
+        Scanner console = new Scanner(System.in);
+        int playerNumber;
         do {
-            Scanner console = new Scanner(System.in);
-            inputNumber = console.nextInt();
-            if (inputNumber > secretNumber) {
+            playerNumber = console.nextInt();
+            if (playerNumber > secretNumber) {
                 System.out.println("Данное число больше того, что загадал компьютер");
-            } else {
+            } else if (playerNumber < secretNumber){
                 System.out.println("Данное число меньше того, что загадал компьютер");
             }
-        } while (inputNumber != secretNumber);
-        if (inputNumber == secretNumber) {
-            System.out.println("Поздравляю, число угадано!");
-        }
+        } while (playerNumber != secretNumber);
+        System.out.println("Поздравляю, число угадано!");
     }
 }
