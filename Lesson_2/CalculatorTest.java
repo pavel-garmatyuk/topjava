83ea7e0c9bce2b4a2a3d5641d743aa00673ccd94
Pavel Garmatyuk <m-krum@yandex.ru>
2021-11-03 13:50:11 +0300

    StartJava: Lesson 2 task#6 Calculator is done

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -3,27 +3,19 @@ import java.util.Scanner;
 public class CalculatorTest {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        String answer = "";
+        String answer = "yes";
         do {
-            System.out.println("Введите первый операнд, затем знак арифметической операции, и второй операнд.\n"
-                    + "Разрешенные знаки арифметической операции: \nСложение + \nВычитание - \nУмножение * \nДеление / "
-                    + "\nВозвести в степень ^ \nПолучить остаток от деления %");
+            if (answer.equalsIgnoreCase("yes")) {
 
-            System.out.println(Calculator.calculate(Integer.parseInt(console.nextLine()),
-                    console.nextLine(), Integer.parseInt(console.nextLine())));
-
-            while (true) {
-                System.out.println("Хотите продолжить вычисления? [yes/no]:");
-                answer = console.nextLine();
-                if (answer.equalsIgnoreCase("yes")) {
-                    break;
-                }
-
-                if (answer.equalsIgnoreCase("no")) {
-                    break;
-                }
+                System.out.println("Введите первый операнд, затем знак арифметической операции, и второй операнд.\n"
+                        + "Разрешенные знаки арифметической операции: \nСложение + \nВычитание - \nУмножение * \nДеление / "
+                        + "\nВозвести в степень ^ \nПолучить остаток от деления %");
+                int result = Calculator.calculate(Integer.parseInt(console.nextLine()),
+                        console.nextLine(), Integer.parseInt(console.nextLine()));
+                System.out.println(result);
             }
-
-        } while ("yes".equals(answer));
+            System.out.println("Хотите продолжить вычисления? [yes/no]:");
+            answer = console.nextLine();
+        } while (!answer.equalsIgnoreCase("no"));
     }
 }
