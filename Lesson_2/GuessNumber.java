545dc19e5766578a76b137f4445572a1d64a2119
Pavel Garmatyuk <m-krum@yandex.ru>
2021-11-14 23:22:56 +0300

    StartJava: Lesson 2 task#7 fix methods is done

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,5 +1,14 @@
 import java.util.Random;
 import java.util.Scanner;
+
+/*GuessNumber
+- после import оставляй пустую строку https://docs.google.com/document/d/1CcFKdXwgnL3h65izX1oY6PtUKQe9DV-I-YEQQ2RKu74/edit#bookmark=id.ynarjmc6p6m8
+- проверки if (makeMove(playerOne) || makeMove(playerTwo)) { можно поместить в while (сюда) {, а внутри его тела выводить какое-то сообщение
+- проверку player.getNumber() == secretNumber; из makeMove помести в checkNumber
+- в методе для проверки чисел для веток с проверкой > и < используй тернарный оператор.
+При этом дублирования кода/текста сообщения не должно быть.
+Данный оператор должен упростить проверку и вывод сообщения, а не усложнить или сделать ее громоздкой или нечитаемой
+checkNumber должен возвращать boolean*/
 public class GuessNumber {
     private final int secretNumber;
     private final Player playerOne;
@@ -12,29 +21,31 @@ public class GuessNumber {
     }
 
     public void play() {
-        while (true) {
-            if (makeMove(playerOne) || makeMove(playerTwo)) {
+        Player player = playerTwo;
+        do {
+            makeMove(playerOne);
+            if (checkNumber(playerOne)) {
+                player = playerOne;
                 break;
+            } else {
+                makeMove(playerTwo);
             }
         }
+        while (!checkNumber(playerTwo));
+        System.out.println("Поздравляю " + player.getName() + ", число угадано!");
     }
 
-    private boolean makeMove(Player player) {
+    private void makeMove(Player player) {
         Scanner input = new Scanner(System.in);
         System.out.println(player.getName() + ", введите число:");
         player.setNumber(input.nextInt());
-        checkNumber(player);
-        return player.getNumber() == secretNumber;
     }
 
-    private void checkNumber(Player player) {
-        if (player.getNumber() > secretNumber) {
-            System.out.println(player.getName() + ", данное число больше того, что загадал компьютер");
-        } else if (player.getNumber() < secretNumber) {
-            System.out.println(player.getName() + ", данное число меньше того, что загадал компьютер");
-        } else {
-            System.out.println("Поздравляю " + player.getName() + ", число угадано!");
-        }
+    private boolean checkNumber(Player player) {
+        String massage = player.getNumber() > secretNumber ? ", данное число больше того, что загадал компьютер" :
+                ", данное число меньше того, что загадал компьютер";
+        System.out.println(player.getName() + massage);
+        return player.getNumber() == secretNumber;
     }
 }
 
