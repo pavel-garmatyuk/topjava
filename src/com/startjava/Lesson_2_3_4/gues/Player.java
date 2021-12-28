009fb1bbed5755db0a6f72fe25f13f4f1de3f052
Pavel Garmatyuk <m-krum@yandex.ru>
2021-12-28 21:24:57 +0300

    StartJava: Lesson 4 task#2 Player fixed rename variables

--- src/com/startjava/Lesson_2_3_4/gues/Player.java
+++ src/com/startjava/Lesson_2_3_4/gues/Player.java
@@ -1,40 +1,39 @@
 package Lesson_2_3_4.gues;
 
-
 public class Player {
     private final String name;
-    private int[] arrayNumber = new int[10];
-    private int indexArray = 0;
+    private int[] numbers = new int[10];
+    private int count = 0;
 
     public Player(String name) {
         this.name = name;
     }
 
-    public void setArrayNumber(int number, int count) {
-        this.arrayNumber[count] = number;
+    public void setNumbers(int number, int count) {
+        this.numbers[count] = number;
     }
 
     public void setCount(int count) {
-        this.indexArray = count;
+        this.count = count;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getIndexArray() {
-        return this.indexArray;
+    public int getCount() {
+        return count;
     }
 
-    public int getArrayNumber(int indexArray) {
-        return this.arrayNumber[indexArray];
+    public int getArrayValue(int count) {
+        return numbers[count];
     }
 
     @Override
     public String toString() {
         var sb = new StringBuilder();
-        for (int i = 0; i <= indexArray; i++) {
-            sb.append(arrayNumber[i]).append(" ");
+        for (int i = 0; i <= count; i++) {
+            sb.append(numbers[i]).append(" ");
         }
         return String.format("Числа игрока %s : %s", name, sb);
     }
